bd22abef7acdcdc3dc4dd35546304ea79b6a3356
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2024-10-25 00:34:40 +0300

    model oluşturulmaya başlandı

---
+++ Data/Register.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduTrack.Data
+{
+    public class Register
+    {
+        [Key]
+        public int RegisterId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public DateTime CourseRegister {  get; set; }
+    }
+}
---
+++ Data/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace EduTrack.Data
+{
+    public class Student
+    {
+        // id; primary key
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
